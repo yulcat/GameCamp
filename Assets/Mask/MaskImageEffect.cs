4e64b093a9d24ff5ed160bac501dc4ee46b04b03
yulcat <alszmtm@naver.com>
2016-08-02 20:19:14 +0900

    Mask 이미지 이펙트

---
+++ Assets/Mask/MaskImageEffect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class MaskImageEffect : MonoBehaviour {
+	public Shader maskShader;
+	private Material maskMaterial;
+    void Awake() {
+		if (!maskShader)
+            Debug.LogError("Shader missing, assign them.");
+		maskMaterial = new Material(maskShader);
+    }
+    void OnRenderImage (RenderTexture source, RenderTexture destination) {
+        Graphics.Blit(source, destination, maskMaterial);
+    }
+
+}
---
+++ Assets/Mask/MaskSprite.shader
@@ -0,0 +1,21 @@
+﻿Shader "Custom/MaskSprite" {
+	Properties {
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+	}
+	SubShader {
+		Pass
+		{
+			Stencil
+            {
+                Ref 5
+                Comp Never
+				Fail Replace
+            }
+
+			AlphaTest Greater 0.1
+            SetTexture [_MainTex] {
+                combine texture * primary, texture
+            }
+		}
+	}
+}
